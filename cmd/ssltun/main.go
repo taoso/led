c3cd7debb4b8e1482c8e175d585fc05a7280b293
海涛 <tao@zz.ac>
2020-02-10 18:10:52 +0800

    support both h2 and h1

--- cmd/ssltun/main.go
+++ cmd/ssltun/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"log"
 	"net/http"
@@ -10,16 +11,26 @@ import (
 )
 
 var name, key string
+var h2 bool
 
 func init() {
 	flag.StringVar(&name, "name", "", "server domain name")
 	flag.StringVar(&key, "key", "", "server auth key")
+	flag.BoolVar(&h2, "h2", false, "enable http/2 protocol")
 }
 
 func main() {
 	flag.Parse()
 
-	ln, err := certmagic.Listen([]string{name})
+	tlsCfg, err := certmagic.TLS([]string{name})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !h2 {
+		tlsCfg.NextProtos = []string{"http/1.1", "acme-tls/1"}
+	}
+
+	ln, err := tls.Listen("tcp", ":443", tlsCfg)
 	if err != nil {
 		log.Fatal(err)
 	}
