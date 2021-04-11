b58c8543caf3bad1d73e8cc23057738916af524c
吕海涛 <ihaitao@qq.com>
2021-04-11 23:11:58 +0800

    init h3

--- cmd/ssltun/main.go
+++ cmd/ssltun/main.go
@@ -4,9 +4,11 @@ import (
 	"crypto/tls"
 	"flag"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
+	"github.com/lucas-clemente/quic-go/http3"
 	"github.com/lvht/ssltun"
 	"golang.org/x/crypto/acme/autocert"
 )
@@ -50,6 +52,26 @@ func main() {
 	if root != "" {
 		proxy.FileHandler = http.FileServer(http.Dir(root))
 	}
+
+	go func() {
+		tlsCfg := acm.TLSConfig()
+		tlsCfg.NextProtos = []string{"h3", "h3-29", "h3-32", "h3-34"}
+
+		ln, err := net.ListenPacket("udp", ":4430")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f := func(w http.ResponseWriter, req *http.Request) {
+			w.Write([]byte("hello h3 and svcb"))
+		}
+
+		h := http.HandlerFunc(f)
+
+		h3 := http3.Server{Server: &http.Server{Handler: h}}
+		h3.TLSConfig = tlsCfg
+		h3.Serve(ln)
+	}()
 
 	if err = http.Serve(ln, proxy); err != nil {
 		log.Fatal(err)
