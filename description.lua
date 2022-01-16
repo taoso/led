803cc7d5c38eccb4c848be4eb7090c2b2f6d7ff6
海涛 <tao@zz.ac>
2022-01-17 00:01:51 +0800

    提取第一段作为摘要

---
+++ description.lua
@@ -0,0 +1,20 @@
+local description = ""
+
+function get_description(blocks)
+	for _, block in ipairs(blocks) do
+		if block.t == 'Para' then
+            description = pandoc.utils.stringify(block)
+            break
+		end
+	end
+
+	return nil
+end
+
+return {{
+	Blocks = get_description,
+	Meta = function (meta)
+        meta.description = description
+		return meta
+	end
+}}
