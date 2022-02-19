6014db3423888cdcb28bc9e73929c50ac150672f
吕海涛 <hi@taoshu.in>
2022-02-19 20:33:36 +0800

    只针对 markdown 目录生成 index.html
    如果删除 md 则自动清理对应的 html。
    可以通过 no_child 环境变量禁止生成子目录 index.html。

--- index.sh
+++ index.sh
@@ -2,13 +2,26 @@
 
 # 为所有文章生成 index.html 文件
 
-diff <(ls $1/*.md 2>/dev/null|sed -E 's/\.md/.html/') <(ls $1/*.html 2>/dev/null) | \
-	grep '>' | \
-	grep -v 'index.html' | \
-	awk '{print $2}' | \
-	xargs rm -f %
+has_md=$(ls $1/*.md 2>/dev/null | head -n 1)
+# 没有 markdown 则不生成 index.html
+if [[ -z "$has_md" ]]; then
+	exit 0
+fi
 
 source ./env
+
+# 支持跳过子目录 index.html
+if [[ ! -z "$no_child" ]]; then
+	if [[ "$1" != "." ]]; then
+		exit 0
+	fi
+fi
+
+# 清理没有对应 markdown 的 html 文件
+diff <(ls $1/*.md 2>/dev/null|sed -E 's/\.md/.html/') <(ls $1/*.html 2>/dev/null) | \
+	grep '>' | \
+	awk '{print $2}' | \
+	xargs rm -f %
 
 echo "title: $site_title" > $1/index.yaml
 echo "site_title: $site_title" >> $1/index.yaml
