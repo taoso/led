7d0257b5f014d8a62c768c26c13016be9aa6cc63
吕海涛 <hi@taoshu.in>
2022-03-25 17:26:09 +0800

    支持自定义作者页面

--- index.sh
+++ index.sh
@@ -28,6 +28,7 @@ echo "site_title: $site_title" >> $1/index.yaml
 echo "site_url: $site_url" >> $1/index.yaml
 echo "author_name: $author_name" >> $1/index.yaml
 echo "author_email: $author_email" >> $1/index.yaml
+echo "author_url: $author_url" >> $1/index.yaml
 echo "articles:" >> $1/index.yaml
 find $1 -name '*.md' -exec meta.sh {} \; | \
 	sort -r |
