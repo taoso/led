1829445302f7d8674fcdd94410ce89d3debf89a5
吕海涛 <hi@taoshu.in>
2022-05-01 14:16:40 +0800

    修复循环构建问题
    远程 md 改动后会触发 FolderCompletion 事件，从而触发构建。但构建
    产生的 html 文件又会触发 FolderCompletion 事件，从而产生死循环。
    
    为此，我们改为监听 RemoteChangeDetected 事件。该事件仅在远程文件
    变更之后才触发。

--- build-all.sh
+++ build-all.sh
@@ -3,22 +3,29 @@
 trap exit SIGINT SIGTERM
 
 since=0
+events=RemoteChangeDetected,PendingDevicesChanged
 
 # 首次启动跳过已经发生的事件
-curl -s -H x-api-key:$api_key $host/rest/events?timeout=0 > /tmp/events.txt
+curl -s -H x-api-key:$api_key "$host/rest/events?timeout=0&events=$events" > /tmp/events.txt
 last=$(jq -r '.[]|.id' /tmp/events.txt | tail -n 1)
 if [[ ! -z "$last" ]]; then
 	since=$last
 fi
 
 while true; do
-        curl -s -H x-api-key:$api_key $host/rest/events?since=$since > /tmp/events.txt
+        curl -s -H x-api-key:$api_key "$host/rest/events?since=$since&events=$events" > /tmp/events.txt
 
 	jq -r '.[]|select(.type == "PendingDevicesChanged")|.data.added[0].deviceID|select(.!=null)' \
 		/tmp/events.txt | sort | uniq | \
 		xargs -I % device.sh %
 
-	jq -r '.[]|select(.type == "FolderCompletion")|.data.folder' \
+	# 如果删除 md 则同步删除对应的 html
+	jq -r '.[]|select(.type == "RemoteChangeDetected")|select(.data.action == "deleted")|"\(.data.folder)/\(.data.path)"' \
+		/tmp/events.txt | sort | uniq | grep -E "\.md$" | sed -E "s/\.md$/.html/" | \
+		xargs -I % rm -f ~/sync/%
+
+	# 目前无法区分新建文件和修改文件，只能无脑触发更新索引页面
+	jq -r '.[]|select(.type == "RemoteChangeDetected")|.data.folder' \
 		/tmp/events.txt | sort | uniq | \
 		xargs -I % build.sh %
 
