3fda212ecfbfa2b24c17306d79eaf32cb41ccf1e
海涛 <tao@zz.ac>
2022-05-02 08:10:20 +0800

    改用更清真的 awk 方案提取 md 信息

--- meta.sh
+++ meta.sh
@@ -2,7 +2,7 @@
 
 # 提取 makdown 文件的 metadata
 
-meta=$(sed -n -e '0,/^$/p' $1)
+meta=$(awk '/^---$/{n++}{if(n==1&&$0!="---"){print $0}if(n==2){exit}}' $1)
 
 file=$(echo $1|sed -E 's/^\.//'|sed -E 's/\.md$/.html/')
 title=$(echo "$meta"|grep title|sed -E 's/\w+:\s+//'|sed 's/"//g')
