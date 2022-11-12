faaf863ff45159f2f1d4a1e888bb7deb14c8f761
海涛 <tao@zz.ac>
2022-11-12 12:21:48 +0800

    统计字数和阅读时间

--- description.lua
+++ description.lua
@@ -1,10 +1,21 @@
 local description = ""
+local runes = 0
 
 function get_description(blocks)
     for _, block in ipairs(blocks) do
         if block.t == 'Para' then
             description = pandoc.utils.stringify(block)
             break
+        end
+    end
+
+    for _, block in ipairs(blocks) do
+        if block.t == 'Para' then
+            local s = pandoc.utils.stringify(block)
+            s,_ = s:gsub("[a-zA-Z-]+","a")
+            s,_ = s:gsub("%s+","")
+
+            runes = runes + utf8.len(s)
         end
     end
 
@@ -15,6 +26,8 @@ return {{
     Blocks = get_description,
     Meta = function (meta)
         meta.description = description
+        meta.runes = string.format("%0.1f", runes/1000)
+        meta.read_time = string.format("%0.1f", runes/400)
         return meta
     end,
     Image = function (img)
