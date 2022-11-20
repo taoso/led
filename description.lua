d7d861d60cf89524598e42d111981dbfed0c0cd1
海涛 <tao@zz.ac>
2022-11-20 19:04:47 +0800

    调整 lua 缩进

--- description.lua
+++ description.lua
@@ -2,36 +2,36 @@ local description = ""
 local runes = 0
 
 function get_description(blocks)
-    for _, block in ipairs(blocks) do
-        if block.t == 'Para' then
-            description = pandoc.utils.stringify(block)
-            break
-        end
+  for _, block in ipairs(blocks) do
+    if block.t == 'Para' then
+      description = pandoc.utils.stringify(block)
+      break
     end
+  end
 
-    for _, block in ipairs(blocks) do
-        if block.t == 'Para' then
-            local s = pandoc.utils.stringify(block)
-            s,_ = s:gsub("[a-zA-Z-]+","a")
-            s,_ = s:gsub("%s+","")
+  for _, block in ipairs(blocks) do
+    if block.t == 'Para' then
+      local s = pandoc.utils.stringify(block)
+      s,_ = s:gsub("[a-zA-Z-]+","a")
+      s,_ = s:gsub("%s+","")
 
-            runes = runes + utf8.len(s)
-        end
+      runes = runes + utf8.len(s)
     end
+  end
 
-    return nil
+  return nil
 end
 
 return {{
-    Blocks = get_description,
-    Meta = function (meta)
-        meta.description = description
-        meta.runes = string.format("%0.1f", runes/1000)
-        meta.read_time = string.format("%0.1f", runes/400)
-        return meta
-    end,
-    Image = function (img)
-        img.attributes.loading = "lazy"
-        return img
-    end,
+  Blocks = get_description,
+  Meta = function (meta)
+    meta.description = description
+    meta.runes = string.format("%0.1f", runes/1000)
+    meta.read_time = string.format("%0.1f", runes/400)
+    return meta
+  end,
+  Image = function (img)
+    img.attributes.loading = "lazy"
+    return img
+  end,
 }}
