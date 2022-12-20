14e7452331d2ac8a080953b3ff6a98ec0a69ca3f
吕海涛 <hi@taoshu.in>
2022-12-20 16:56:32 +0800

    优化中文环境下软换行展示效果
    Markdown 中默认会把软换行转成空格，比如下面的内容
    
      a
      b
    
    会转换成`<p>a b</p>`。这在英文环境下没有任何问题。
    但换成中文就很难看了，比如：
    
      你
      好
    
    会转换成`<p>你 好</p>`，汉字中间多了个空格。
    
    之前写中文 Markdown 只能强忍着不换行，可读性比较差。
    
    今天借助 pandoc lua-filter 自动去掉汉字之间的空格。

--- description.lua
+++ description.lua
@@ -1,6 +1,11 @@
 local description = ""
 local runes = 0
 
+function is_ascii(char)
+  local ascii_code = string.byte(char)
+  return ascii_code >= 0 and ascii_code <= 127
+end
+
 function get_description(blocks)
   for _, block in ipairs(blocks) do
     if block.t == 'Para' then
@@ -78,5 +83,21 @@ return {{
       link.target = t:sub(1,-3).."html"
     end
     return link
+  end,
+  Para = function (para)
+    local cs = para.content
+    for k, v in ipairs(cs) do
+      if v.t == 'SoftBreak' then
+        local p = cs[k-1].text
+        local n = cs[k+1].text
+
+        if p ~= nil and n ~= nil then
+          if (not is_ascii(p:sub(-1))) and (not is_ascii(n:sub(1,1))) then
+            para.content[k] = pandoc.Str("")
+          end
+        end
+      end
+    end
+    return para
   end,
 }}
