6174b37494da5eb748f17fbf71f78356e5d71644
吕海涛 <hi@taoshu.in>
2023-04-20 08:23:53 +0800

    重新为百度生成 sitemap 文件😂

--- cmd/mkd/map.sh
+++ cmd/mkd/map.sh
@@ -10,10 +10,10 @@ cat << EOF
 EOF
 
 # - {"path":"/go/monkey.html","title":"Go语言实现猴子补丁","date":"2021-08-28"}
-grep '^- {"path":' $1/index.yml | while read line; do
-	path=$(echo $line|cut -d\" -f4)
+grep '^- ' $1/index.yml | while read line; do
+	path=$(echo $line|cut -d\" -f8)
 	link="$site_url$path"
-	updated=`date -u +"%Y-%m-%dT%H:%M:%SZ" -r $1$path`
+	updated=`date -u +"%Y-%m-%dT%H:%M:%SZ" -r $1${path/%html/htm}`
 
 	cat << EOF
   <url>
