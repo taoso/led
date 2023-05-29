86b1320a0653c0c5a5ea57d04481a37bfc9442e1
吕海涛 <hi@taoshu.in>
2023-05-29 18:25:06 +0800

    全量生成文章索引
    基础 Makefile 的增量实现有问题，目前文章子列表无法更新。

--- cmd/mkd/index.sh
+++ cmd/mkd/index.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-dir=$(dirname $1)
+find $1 -type d | while read dir; do
+
+no_child=""
 
 if [[ -f $dir/env ]]; then
 	set -o allexport
@@ -11,11 +13,11 @@ fi
 # 支持跳过子目录 index.htm
 if [[ ! -z "$no_child" ]]; then
 	if [[ "$dir" != "." ]]; then
-		exit 0
+		continue
 	fi
 fi
 
-tmp=$(mktemp).yml
+tmp=$dir/index.yml
 # index.htm 没有对应的 markdown 文件件
 # 所以 title 变量为空，pandoc 会输出警告信息
 echo "title: no_warn" > $tmp
@@ -23,30 +25,24 @@ echo "site_title: $site_title" >> $tmp
 echo "articles:" >> $tmp
 
 find $dir -name '*.yml' \
-	! -name "draft-*.yml" ! -name "index.yml" ! -name "feed.yml" \
+	! -name "draft-*.yml" \
+	! -name "index.yml" \
+	! -name "feed.yml" \
 	-exec cat {} + | sort -r >> $tmp
 
 # 没有 markdown 则不生成 index.htm
 if [[ "$(tail -n 1 $tmp)" == "articles:" ]]; then
 	rm $tmp
-	exit 0
+	continue
 fi
 
-meta=$dir/index.yml
-
-diff -u $tmp $meta > /dev/null
-
-if [ $? -eq 0 ]; then
-	rm $tmp
-	exit 0
-fi
-
-mv $tmp $meta
+index=$dir/index.htm
 
 pandoc -s -p -f markdown --wrap=none \
 	--template index.tpl \
-	--metadata-file=$meta \
+	--metadata-file=$tmp \
 	--lua-filter $LUA_FILTER \
-	-o $1 /dev/null
+	-o $index /dev/null
 
-[ -s $1 ] || rm $1
+[ -s $index ] || rm $index
+done
