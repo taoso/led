ff5116161a1933d596789f10ea0f9712add02114
吕海涛 <hi@taoshu.in>
2023-05-30 10:56:18 +0800

    支持在 macos 平台生成元信息

--- cmd/mkd/meta.sh
+++ cmd/mkd/meta.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-updated=`date -u +"%Y-%m-%dT%H:%M:%SZ" -r $1`
+if [[ $(uname -s) == "Darwin" ]]; then
+	updated=`date -jf "%s" "$(stat -f "%m" $1)" "+%Y-%m-%dT%H:%M:%SZ"`
+else
+	updated=`date -u +"%Y-%m-%dT%H:%M:%SZ" -r $1`
+fi
 
 pandoc -s -p -f markdown --wrap=none \
 	--template $ROOT_DIR/meta.tpl \
