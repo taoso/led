70099c741607a41f73db5f828e50c00a5fcd7ddd
吕海涛 <hi@taoshu.in>
2023-05-30 10:56:53 +0800

    使用 pandoc 模板生成 sitemap

--- cmd/mkd/feed.sh
+++ cmd/mkd/feed.sh
@@ -5,14 +5,27 @@
 
 updated=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 
+if [[ -f $1/env ]]; then
+	set -o allexport
+	source $1/env
+	set +o allexport
+fi
+
 # Feed 最多保留十条记录
 # 但头两行是 tiltle 和 articles
 # 所以从第三行开始
 head -n 12 $1/index.yml > $1/feed.yml
 
-pandoc -s -p -f markdown --wrap=none \
+pandoc -s -p -f markdown -t html --wrap=none \
 	--metadata=updated:$updated \
 	--metadata-file=$1/feed.yml \
 	--template $ROOT_DIR/feed.tpl \
 	--lua-filter $LUA_FILTER \
-	-o - /dev/null
+	-o $1/feed.xml /dev/null
+
+pandoc -s -p -f markdown -t html --wrap=none \
+	--metadata=updated:$updated \
+	--metadata-file=$1/index.yml \
+	--template $ROOT_DIR/map.tpl \
+	--lua-filter $LUA_FILTER \
+	-o $1/map.xml /dev/null
