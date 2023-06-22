70cc9ba8114d4259a24009c58efbdcc9d8f2d5bf
吕海涛 <hi@taoshu.in>
2023-06-22 11:16:53 +0800

    支持 dot/tex/asy 绘图

--- cmd/mkd/desc.lua
+++ cmd/mkd/desc.lua
@@ -49,7 +49,197 @@ function realpath(a, b)
   return c:reverse()..b
 end
 
+-- Report Lua warnings to stderr
+if warn then
+  warn '@on'
+else
+  warn = function (...) io.stderr:write(string.concat({...})) end
+end
+
+local system = require 'pandoc.system'
+local with_temporary_directory = system.with_temporary_directory
+local with_working_directory = system.with_working_directory
+
+local function read_file (filepath)
+  local fh = io.open(filepath, 'rb')
+  local contents = fh:read('a')
+  fh:close()
+  return contents
+end
+
+local function write_file (filepath, content)
+  local fh = io.open(filepath, 'wb')
+  fh:write(content)
+  fh:close()
+end
+
+--- GraphViz engine
+local graphviz = {
+  line_comment_start = '//',
+  compile = function (self, code)
+    return pandoc.pipe('dot', {"-Tsvg"}, code)
+  end,
+}
+
+--- XeLaTeX engine
+local xelatex = {
+  line_comment_start = '%%',
+  compile = function (self, code, user_opts)
+    return with_temporary_directory('tex', function (tmpdir)
+      return with_working_directory(tmpdir, function ()
+        code = "\\documentclass[dvisvgm]{standalone}\n" .. code
+        write_file('draw.tex', code)
+
+        local args = {'-halt-on-error', '-no-pdf', 'draw.tex'}
+        pandoc.pipe('xelatex', args, '')
+        pandoc.pipe('dvisvgm', {'draw.xdv'}, '')
+
+        return read_file('draw.svg')
+      end)
+    end)
+  end
+}
+
+--- Asymptote diagram engine
+local asymptote = {
+  line_comment_start = '//',
+  compile = function (self, code)
+    return with_temporary_directory('asymptote', function(tmpdir)
+      return with_working_directory(tmpdir, function ()
+        local args = {'-tex', 'xelatex', '-f', 'svg', '-o', 'draw', '-'}
+        pandoc.pipe('asy', args, code)
+        return read_file('draw.svg')
+      end)
+    end)
+  end,
+}
+
+local engines = {
+  ['asy.svg'] = asymptote,
+  ['dot.svg'] = graphviz,
+  ['tex.svg'] = xelatex,
+}
+
+local function properties_from_code (code, comment_start)
+  local props = {}
+  local pattern = comment_start:gsub('%p', '%%%1') .. '|%s+' ..
+    '([-_%w]+):%s+([^\n]*)\n'
+  for key, value in code:gmatch(pattern) do
+    if key == 'fig-cap' then
+      props['caption'] = value
+    else
+      props[key] = value
+    end
+  end
+  return props
+end
+
+local function diagram_options (cb, comment_start)
+  local attribs = comment_start
+    and properties_from_code(cb.text, comment_start)
+    or {}
+  for key, value in pairs(cb.attributes) do
+    attribs[key] = value
+  end
+
+  -- Read caption attribute as Markdown
+  local caption = attribs.caption
+    and pandoc.read(attribs.caption).blocks
+    or nil
+  local fig_attr = {
+    id = cb.identifier ~= '' and cb.identifier or attribs.label,
+    name = attribs.name,
+  }
+  local user_opt = {}
+
+  for k, v in pairs(attribs) do
+    local prefix, key = k:match '^(%a+)%-(%a[-%w]*)$'
+    if prefix == 'fig' then
+      fig_attr[key] = v
+    elseif prefix == 'opt' then
+      user_opt[key] = v
+    end
+  end
+
+  return {
+    ['alt'] = attribs.alt or
+      (caption and pandoc.utils.blocks_to_inlines(caption)) or
+      {},
+    ['caption'] = caption,
+    ['fig-attr'] = fig_attr,
+    ['filename'] = attribs.filename,
+    ['image-attr'] = {
+      height = attribs.height,
+      width = attribs.width,
+      style = attribs.style,
+      loading = 'lazy',
+    },
+    ['opt'] = user_opt,
+  }
+end
+
+local draws = 0
+
+-- Executes each document's code block to find matching code blocks:
+local function code_to_figure (block)
+  -- Check if a converter exists for this block. If not, return the block
+  -- unchanged.
+  local diagram_type = block.classes[1]
+  if not diagram_type then
+    return nil
+  end
+
+  local engine = engines[diagram_type]
+  if not engine then
+    return nil
+  end
+
+  -- Unified properties.
+  local dgr_opt = diagram_options(block, engine.line_comment_start)
+  for optname, value in pairs(engine.opt or {}) do
+    dgr_opt.opt[optname] = dgr_opt.opt[optname] or value
+  end
+
+  -- No cached image; call the converter
+  local ok, imgdata = pcall(engine.compile, engine, block.text, dgr_opt.opt)
+
+  -- Bail if an error occurred; imgdata contains the error message
+  -- when that happens.
+  if not ok then
+    warn(PANDOC_SCRIPT_FILE, ': ', tostring(imgdata))
+    return nil
+  elseif not imgdata then
+    warn(PANDOC_SCRIPT_FILE, ': Diagram engine returned no image data.')
+    return nil
+  end
+
+  -- Use the a name by hashing the image content.
+  draws = draws + 1
+  local basename, _ = pandoc.path.split_extension(PANDOC_STATE.input_files[1])
+  local fname = basename .. '.' .. draws .. '.svg'
+
+  if not dgr_opt.opt['embed-font'] then
+    imgdata = string.gsub(str, "\n<defs>.-</defs>\n", "")
+  end
+
+  write_file(fname, imgdata)
+
+  -- Create the image object.
+  local image = pandoc.Image(dgr_opt.alt, fname, "", dgr_opt['image-attr'])
+
+  -- Create a figure if the diagram has a caption; otherwise return
+  -- just the image.
+  return dgr_opt.caption and
+    pandoc.Figure(
+      pandoc.Plain{image},
+      dgr_opt.caption,
+      dgr_opt['fig-attr']
+    ) or
+    pandoc.Plain{image}
+end
+
 return {{
+  CodeBlock = code_to_figure,
   Blocks = get_description,
   Meta = function (meta)
     meta.description = description
