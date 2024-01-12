a72a212e13228db16ec32b32db2a771c1b0e043a
海涛 <tao@zz.ac>
2024-01-12 09:03:24 +0800

    基于 WebDAV 事件来生成博客

--- cmd/mkd/build-all.sh
+++ cmd/mkd/build-all.sh
@@ -2,35 +2,26 @@
 
 trap exit SIGINT SIGTERM
 
-since=0
-events=RemoteChangeDetected,PendingDevicesChanged
-
-# 首次启动跳过已经发生的事件
-curl -s -H x-api-key:$api_key "$host/rest/events?timeout=0&events=$events" > /tmp/events.txt
-last=$(jq -r '.[]|.id' /tmp/events.txt | tail -n 1)
-if [[ ! -z "$last" ]]; then
-	since=$last
-fi
-
 while true; do
-        curl -s -H x-api-key:$api_key "$host/rest/events?since=$since&events=$events" > /tmp/events.txt
-
-	jq -r '.[]|select(.type == "PendingDevicesChanged")|.data.added[0].deviceID|select(.!=null)' \
-		/tmp/events.txt | sort | uniq | \
-		xargs -I % device.sh %
-
-	# 如果删除 md 则同步删除对应的 htm/yml
-	jq -r '.[]|select(.type == "RemoteChangeDetected")|select(.data.action == "deleted")|"\(.data.folder)/\(.data.path)"' \
-		/tmp/events.txt | sort | uniq | grep -E "\.md$" | sed -E "s/\.md$//" | \
-		xargs -I % rm -f ~/sync/%.{htm,yml}
-
-	# 目前无法区分新建文件和修改文件，只能无脑触发更新索引页面
-	jq -r '.[]|select(.type == "RemoteChangeDetected")|.data.folder' \
-		/tmp/events.txt | sort | uniq | \
-		xargs -I % build.sh %
-
-	last=$(jq -r '.[]|.id' /tmp/events.txt | tail -n 1)
-	if [[ ! -z "$last" ]]; then
-		since=$last
-	fi
+	curl -s -u $auth "$host/+/dav-events?d=60s" | sort | uniq | \
+		while read ev; do
+			if [[ $ev == -* ]]; then
+				# 删除 MD 清理对应生成的文件
+				# -lehu.in/a/b.md -> lehu.in/a/b
+				f=${ev#-}
+				f=${f%.md}
+				rm -f ~/sync/$f.{htm,yml}
+				# 提取域名
+				# -lehu.in/a/b.md -> lehu.in
+				ev=${ev%%/*}
+				ev=${ev#-}
+			else
+				# 新增或修改 MD 文件
+				# +lehu.in -> lehu.in
+				ev=${ev#+}
+			fi
+			# 重新增量构建
+			build.sh $ev &
+		done
+	done
 done
