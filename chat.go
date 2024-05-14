1a0d535110aa4246563e086654c81a5d78326217
海涛 <tao@zz.ac>
2024-05-14 17:11:53 +0800

    添加 gpt-4o 模型

--- chat.go
+++ chat.go
@@ -141,6 +141,10 @@ func (p *Proxy) chat(w http.ResponseWriter, req *http.Request, f *FileHandler) {
 		tokenRate = 6
 		msg.Model = "gpt-4-turbo"
 		maxTokens = 4 * 1024
+	case "gpt-4o":
+		tokenRate = 6
+		msg.Model = "gpt-4o"
+		maxTokens = 4 * 1024
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("invalid model"))
